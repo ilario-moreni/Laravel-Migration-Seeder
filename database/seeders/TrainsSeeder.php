d360a89e0eb86f8a985494f141594fe603c75c05
ilario-moreni <ilario2001.mor@gmail.com>
2023-02-21 16:03:55 +0100

    broken code

---
+++ database/seeders/TrainsSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+class TrainsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<=10; $i++){
+            $newTrain = new TrainsSeeder();
+            $newTrain->azienda = $faker->word();
+            $newTrain->stazione_di_partenza = $faker->word();
+            $newTrain->stazione_di_arrivo = $faker->word();
+            $newTrain->orario_di_partenza = $faker->time();
+            $newTrain->orario_di_arrivo = $faker->time();
+            $newTrain->codice_treno = $faker->md5();
+            $newTrain->numero_carrozze = $faker->numberBetween(3, 20);
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+            $newTrain->save();
+        }
+    }
+
+    
+}
